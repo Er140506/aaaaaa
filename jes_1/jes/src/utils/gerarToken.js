bd7a095c4f0397c007cf650dfb0ba020256c1256
erik douglas <e.lago@aluno.senai.br>
2026-08-18 08:21:08 -0300

    feat:token

--- jes_1/jes/src/utils/gerarToken.js
+++ jes_1/jes/src/utils/gerarToken.js
@@ -4,6 +4,6 @@ export const gerarToken = (usuario) => {
     return jwt.sign(
         { id: usuario.id, email: usuario.email, tipo: usuario.tipo },
         process.env.JWT_SECRET,
-        { expiresIn: process.env.JWT_EXPIRES_IN || "1d" }
+        { expiresIn: process.env.JWT_EXPIRES_IN || "12h" }
     )
 }
